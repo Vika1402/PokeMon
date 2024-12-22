3564645dff9b42e53de45f8a76013cce816e7d62
Vika1402 <vikas.kumar123@ssipmt.com>
2024-12-22 19:06:19 +0530

    Pokemon web App starting

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div>Pokemon</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust based on your project structure
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        jakarta: ['"Plus Jakarta Sans"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
